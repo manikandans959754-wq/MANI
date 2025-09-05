431c7779892e9ccfa8465ec0b6bcfd701a3f9852
manikandans959754-wq <manikandan.s959754@gmail.com>
2025-09-05 10:43:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Mobile menu toggle
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector("nav ul");
-
-menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
-
