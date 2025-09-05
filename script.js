e2679ff4fcd052620ec61ea3199a77d3481a36b6
manikandans959754-wq <manikandan.s959754@gmail.com>
2025-09-05 10:42:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
